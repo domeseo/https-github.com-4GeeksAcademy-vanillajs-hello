db3001852e4827b5cc7ac490ff5e799f6a11ac8a
Domenico Puzone <137282500+domeseo@users.noreply.github.com>
2025-01-11 13:04:16 +0000

    hola

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
+/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let goalkeeper = [];
+let defense = [];
+let midfielder = [];
+let striker = [];
+
+function getContent(ref_goalkeeper, ref_defensa, ref_mediocampista, ref_atacante) {
+
+  const porteroInput = document.getElementById('suputamadre');
+  const portero = porteroInput.value;
+  porteroInput.value = "";
+  const defenderInput = document.getElementById('defensa');
+  const defender = defenderInput.value;
+  defenderInput.value = "";
+  const midinput = document.getElementById('mediocampista');
+  const mid = midinput.value;
+  midinput.value = "";
+  const strikeInput = document.getElementById('atacante');
+  const strike = strikeInput.value;
+  strikeInput.value = "";
+
+
+  ref_goalkeeper.push(portero);
+  ref_defensa.push(defender);
+  ref_mediocampista.push(mid);
+  ref_atacante.push(strike);
 };
+
+let button = document.getElementById("submit");
+
+button.addEventListener("click", () => {  
+  getContent(goalkeeper, defense, midfielder, striker);
+  console.log(goalkeeper, defense, midfielder, striker);
+
+// sustituye los elementos del team
+
+let porta = document.getElementById('resultadogk');
+porta.innerText = goalkeeper[goalkeeper.length - 1]; 
+
+let retro = document.getElementById('resultadodf');
+retro.innerHTML = defense[defense.length - 1];
+
+let centro = document.getElementById('resultadomd');
+centro.innerHTML = midfielder[midfielder.length - 1];
+
+let att = document.getElementById('resultadost');
+att.innerHTML = striker[striker.length -1];
+
+});
+
+
